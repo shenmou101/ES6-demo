e9d39fb0beead140d5ae8d2019a477e7c3ede05d
shenmou101 <516211584@qq.com>
2016-10-20 08:36:40 +0800

    变量定义和结构赋值

--- 2.结构赋值/1.数组的结构赋值.js
+++ 2.结构赋值/1.数组的结构赋值.js
@@ -1,3 +1,19 @@
 /**
  * Created by 51621 on 2016/10/19.
  */
+//结构赋值 变量的批量赋值
+'use strict';
+
+let arr = [1, , 3];
+let arr2 = [1, [2.1,2.2], 3];
+/*
+//传统赋值方式
+let a = arr[0];
+let b = arr[1];
+let c = arr[2];
+*/
+
+let [a, b, c] = arr;
+let [e, f, g] = arr2;
+console.log(a,b,c);
+console.log(e, f, g);
